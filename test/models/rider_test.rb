c4484239216360bb8b965806d3e80213785b1f13
eiji <eijimine@gmail.com>
2017-08-03 20:24:21 -0400

    Part 4 complete

--- test/models/rider_test.rb
+++ test/models/rider_test.rb
@@ -7,8 +7,7 @@ class RiderTest < ActiveSupport::TestCase
     # this time use create( ... ) instead of build
     aegon = create(:rider, name: 'Aegon', sword: 'Blackfyre')
     daenerys = create(:rider, name: 'Daenerys', sword: 'Lightbringer')
-
-    # can't change the assert_ statements!
+    #  can't change the assert_ statements!
     skip unless aegon && daenerys
     assert_equal('Daenerys brings Lightbringer crashing down!', daenerys.swing)
     assert_equal('Aegon brings Blackfyre crashing down!', aegon.swing)
